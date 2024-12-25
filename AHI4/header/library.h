97f49f08ca3e41cdc63ce42d96b20d65ec22bbb7
Christoph Fassbach <christoph.fassbach@gmail.com>
2024-12-25 20:37:15 +0100

    Bump to next version - Alpha11

--- AHI4/header/library.h
+++ AHI4/header/library.h
@@ -47,9 +47,9 @@
  *****************************************************************************/
 #define LIBRARY_NAME      "AmiGUS.audio"
 #define LIBRARY_VERSION   4
-#define LIBRARY_REVISION  10
+#define LIBRARY_REVISION  11
 #define LIBRARY_DATETXT	  __AMIGADATE__
-#define LIBRARY_VERSTXT	  "4.010"
+#define LIBRARY_VERSTXT	  "4.011"
 
 #ifdef _M68060
   #define LIBRARY_ADDTXT  " 060"

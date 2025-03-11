e1cb99110ea66b3fc318004bc79b9213ee9f13b4
Christoph Fassbach <christoph.fassbach@gmail.com>
2025-03-11 10:12:00 +0100

    MHI: add missing copyright notice

--- MHI/test/test_playback.c
+++ MHI/test/test_playback.c
@@ -1,3 +1,19 @@
+/*
+ * This file is part of the mhiAmiGUS.library.
+ *
+ * mhiAmiGUS.library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, version 3 of the License only.
+ *
+ * mhiAmiGUS.library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU LesserGeneral Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with mhiAmiGUS.library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>

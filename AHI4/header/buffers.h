17f500368688ab2c1957fcb65f18a8f4c5350966
Christoph Fassbach <christoph.fassbach@gmail.com>
2025-05-04 15:09:50 +0200

    AHI: Fix typos in licensing info

--- AHI4/header/buffers.h
+++ AHI4/header/buffers.h
@@ -7,11 +7,13 @@
  *
  * AmiGUS.audio driver is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU LesserGeneral Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with AmiGUS.audio driver.  If not, see <http://www.gnu.org/licenses/>.
+ * along with AmiGUS.audio driver.
+ *
+ * If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef BUFFERS_H
